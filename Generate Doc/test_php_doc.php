f567363c48b26b6eee6e4aa66f3cb09292b2d35d
FruHafizd <muhammadhafidz12333@gmail.com>
2025-05-16 01:08:23 +0800

    Added Generate Doc

--- Generate Doc/test_php_doc.php
+++ Generate Doc/test_php_doc.php
@@ -216,18 +216,3 @@ EOH;
 		return true; 
 	} 
 }
-
-
-
-// Load library 
-//include_once 'HtmlToDoc.class.php';  
-
-// Initialize class 
-$htd = new HTML_TO_DOC();
-
-
-$htmlContent = ' 
-<h1>Hello World!</h1> 
-<p>This document is created from HTML.</p>';
-
-$htd->createDoc($htmlContent, "my-document.doc");
